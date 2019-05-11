c81fc5584efa09562abe758061a272bb431de923
SUMUKHA-PK <sumukhapk46@gmail.com>
2019-05-11 14:16:45 +0530

    Updates

---
+++ precache-manifest.4bf1b71534a35c397f03b9a7bddcf82f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535ba36b65c01de482a9d54ea874f025",
+    "url": "/GetServiceClient/index.html"
+  },
+  {
+    "revision": "68712ddc4aa07c42d8af",
+    "url": "/GetServiceClient/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/GetServiceClient/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "68712ddc4aa07c42d8af",
+    "url": "/GetServiceClient/static/js/main.f07483b8.chunk.js"
+  },
+  {
+    "revision": "b01c1d005a753c8e8953",
+    "url": "/GetServiceClient/static/js/runtime~main.08bfcc00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GetServiceClient/static/media/logo.5d5d9eef.svg"
+  }
+]);
