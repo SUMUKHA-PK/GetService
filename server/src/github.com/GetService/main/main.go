f7d539366292e6172a06b91d83f291a9637acedc
SUMUKHA-PK <sumukhapk46@gmail.com>
2019-05-23 20:16:16 +0530

    Revert "Basic React App added and basics of a server set up"
    This reverts commit 160f115003da368d06f6e67ba3b6c89e9ae4298c.

--- server/src/github.com/GetService/main/main.go
+++ server/src/github.com/GetService/main/main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
+import "fmt"
 
-	"../routing"
-)
-
-func main() {
-	go func() {
-		mux := &http.ServeMux{}
-		mux.HandleFunc("/", routing.HandlerFunc)
-		fmt.Print("Web server Serving on port 3000\n")
-		http.ListenAndServe("192.168.43.10:3000", mux)
-	}()
-
-	fmt.Scanln()
-	fmt.Scanln()
-	fmt.Print("Server shut down")
+func main(){
+	fmt.Println("Hello world!")
 }
